988b849ba1d3142119194c05f649e53554747b7b
Abhinav Anand <cu.22bcs15064@gmail.com>
2025-02-21 17:17:57 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/bfhl', methods=['POST'])
+def process_data():
+    try:
+        data = request.json.get("data", [])
+        numbers = [x for x in data if x.isdigit()]
+        alphabets = [x for x in data if x.isalpha()]
+        highest_alphabet = [max(alphabets, key=str.lower)] if alphabets else []
+
+        response = {
+            "is_success": True,
+            "user_id": "abhinav_anand_21022025",  # Use your name & DOB
+            "email": "your_email@example.com",
+            "roll_number": "Your_Roll_Number",
+            "numbers": numbers,
+            "alphabets": alphabets,
+            "highest_alphabet": highest_alphabet
+        }
+        return jsonify(response), 200
+    
+    except Exception as e:
+        return jsonify({"is_success": False, "error": str(e)}), 400
+
+@app.route('/bfhl', methods=['GET'])
+def get_operation_code():
+    return jsonify({"operation_code": 1}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
